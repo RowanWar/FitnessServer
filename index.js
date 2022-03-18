0ad0b625a62bc9625e9628b495fdc94d90256974
Rowan <up881930@myport.ac.uk>
2022-03-18 02:14:02 +0000

    Added pool db file, wrote some code for equipment query functionality

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const pool = require('./pool.js')
 const app = express()
 
 app.get('/', async (req, res) => {
-  res.send('OK WORLD')
+  pool.query('SELECT * FROM equipment', (err, results) => {
+    if (err) {
+      res.json('Error encountered!');
+      console.log('Error encountered: ' + err); // Returns error to console only for securiy purposes
+      return;
+    }
+
+    let data = results.rows // Assignsn shorter identifier to results
+    console.log(data);
+    res.status(200).json(data)
+  })
 })
 
 app.listen(8080, () => { console.log('Server established on port 8080')})
