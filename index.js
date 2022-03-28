66053ac343edf9a2ec6f50924683c2ab711692ab
Rowan <up881930@myport.ac.uk>
2022-03-28 17:27:06 +0100

    Added new get all users query

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const pool = require('./pool.js')
 const app = express()
 
 app.get('/', async (req, res) => {
-  res.send('OK WORLD')
+  res.send('STATUS 200')
 });
 
 app.get('/api/getEquipment', async (req, res) => {
   pool.query('SELECT * FROM equipment', (err, results) => {
+    if (err) {
+      res.json('Error encountered!');
+      console.log('Error encountered: ' + err); // Returns error to console only for securiy purposes
+      return;
+    }
+
+    let data = results.rows // Assignsn shorter identifier to results
+    console.log(data);
+    res.status(200).json(data)
+  })
+});
+
+app.get('/api/getUser', async (req, res) => {
+  pool.query('SELECT * FROM user', (err, results) => {
     if (err) {
       res.json('Error encountered!');
       console.log('Error encountered: ' + err); // Returns error to console only for securiy purposes
