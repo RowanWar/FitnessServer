0c6b889dc7607147809d7e14b208b06381c5dece
Rowan <up881930@myport.ac.uk>
2022-04-05 20:21:55 +0100

    Testing insert put req

--- index.js
+++ index.js
@@ -77,38 +77,46 @@ app.get('/api/getReservation/:id', async (req, res) => {
 
 app.put('/api/updateReservation/:id', async (req, res) => {
   const id = req.params.id;
-  const updatedReservation = req.body;
+  // const updatedReservation = req.body;
 
-  try {
+  query = 'INSERT INTO reservation(equip_id, user_id, cat_name, category_desc) VALUES($1, $2, $3, $4) RETURNING *'
+  values = ['6', '1', 'Categ name', 'Categ description']
+  pool.query(query, values, (err, res) => {
+    if (err) {
+      console.log(err.stack)
+    }
+    else {
+      console.log(res.rows[0]);
+      res.status(200).json(results.rows);
+    }
+  })
+
     //SELECT * FROM equipment e JOIN equipment_type et ON e.equip_type_id = et.equip_type_id ORDER BY e.equip_type_id, equip_id ASC;
     // Queries the reservation via passed ID param above
     // pool.query('SELECT * FROM reservation r JOIN equipment e ON r.equip_id = e.equip_id WHERE reservation_id = $1 ORDER BY r.reserve_time DESC', [id], (err, results) => {
     // pool.query(UPDATE "reservation")
-    pool.query(INSERT INTO reservation(equip_id, user_id, cat_name, category_desc) VALUES('6', '1', 'Categ name', 'Categ description'), (err, results) => {
-
-      if (err) {
-        // Returns detailed error to console only for securiy reasons.
-        console.log('Error encountered: ' + err);
-        return (res.json('Error encountered!'));
-      }
-
-      else if (results.rows.length === 0) {
-        console.log('No reservation with this ID');
-        res.status(200).json('No reservation with this ID');
-      }
-
-      else {
-        // res.status(200).json(results.rows.reservation_id);
-        // res.status(200).json(results.rows);
-        // results.rows.is_available = updatedReservation;
-        res.status(200).json(results.rows);
-      }
-    })
+    // pool.query(INSERT INTO reservation()VALUES(), (err, results) => {
+    //
+    //   if (err) {
+    //     // Returns detailed error to console only for securiy reasons.
+    //     console.log('Error encountered: ' + err);
+    //     return (res.json('Error encountered!'));
+    //   }
+    //
+    //   else if (results.rows.length === 0) {
+    //     console.log('No reservation with this ID');
+    //     res.status(200).json('No reservation with this ID');
+    //   }
+    //
+    //   else {
+    //     // res.status(200).json(results.rows.reservation_id);
+    //     // res.status(200).json(results.rows);
+    //     // results.rows.is_available = updatedReservation;
+    //     res.status(200).json(results.rows);
+    //   }
+    // })
   }
-  catch (catchError) {
-    console.error(catchError);
-  }
-});
+);
 
 
 app.listen(8080, () => { console.log('Server established on port 8080')})
