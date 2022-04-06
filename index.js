8c0675e8802c24abafc0fdf12a17724f5b687e95
Rowan <up881930@myport.ac.uk>
2022-04-06 01:30:54 +0100

    Adding check to reservation if equipment is available

--- index.js
+++ index.js
@@ -89,13 +89,13 @@ app.put('/api/createReservation/:id', async (req, res) => {
   const equipmentQueryVals = [false, id];
 
   pool.query(checkEquipAvailable, checkEquipAvailableVals)
-      .then (res => {
+      .then (res.rows => {
         if (res.rows = !true) {
           console.log('If statement ran');
           res.status(200).json('This equipment is already reserved!');
           return;
         }
-        console.log('First query = ' + typeof(res.rows));
+        console.log('First query = ' + res.rows);
         pool.query(reservationQuery, reservationQueryVals)
         console.log('1st promise')
         .then (res2 => {
