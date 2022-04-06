fac1b15a3778f356a3ddc24d52590abe6d035465
Rowan <up881930@myport.ac.uk>
2022-04-06 02:08:24 +0100

    Adding check to reservation if equipment is available

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.put('/api/createReservation/:id', async (req, res) => {
         //   return;
         // }
         console.log('First query = ' + resp.rows);
-        console.log(resp[0]);
+        console.log(resp.is_available[0]);
         // pool.query(reservationQuery, reservationQueryVals)
         console.log('1st promise')
         .then (res2 => {
