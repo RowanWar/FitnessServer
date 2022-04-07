3332cea9c83257d7bdf8a062ad224855e54d89ba
Rowan <up881930@myport.ac.uk>
2022-04-07 02:35:24 +0100

    Testing

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.put('/api/createReservation/:id', async (req, res) => {
   pool.query(checkEquipAvailable, checkEquipAvailableVals)
       .then (response => {
         console.log(JSON.stringify(response.rows[0]));
-        console.log(Object.keys(response.rows[0]));
+        console.log(Object.values(response.rows[0]));
         console.log(typeof(response.rows))
         if (response.rows == false) {
           console.log('If statement ran');
