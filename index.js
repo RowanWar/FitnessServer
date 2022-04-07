772fc5bdfa50db9c2899cf2eb0c6aaa9cea05f45
Rowan <up881930@myport.ac.uk>
2022-04-07 03:50:27 +0100

    Testing

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ app.put('/api/createReservation/:id', async (req, res) => {
 
         // Converts returned query data from checkEquipAvailable into a string for the comparison if statement below
         // const equipmentIsAvailable = JSON.stringify(Object.values(response.rows[0]));
-        const equipmentIsAvailable = response.rows[0];
+        const test = response.rows[0];
+        const equipmentIsAvailable = test["is_available"];  
         console.log(typeof(equipmentIsAvailable));
         // const equipmentIsAvailable.slice(1)
         console.log(equipmentIsAvailable);
