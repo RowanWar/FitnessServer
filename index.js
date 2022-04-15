b3a587b323e0e6d0b735aa967203a4fb4e541ca5
Rowan <up881930@myport.ac.uk>
2022-04-15 03:26:59 +0100

    Adding additional functionality to getEquipment api call

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/api/getEquipment/:userId', async (req, res) => {
             res.status(200).json('No reservation for user: ' + id);
             return
           }
-          res.status(200).json(response.rows, nextResponse.rows);
+          res.status(200).json(response.rows + nextResponse.rows);
         })
       });
       // .catch(e => console.error(e.stack))
