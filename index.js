8d1253150755ed4c5c97baa1ef615bbc19b5bc1f
Rowan <up881930@myport.ac.uk>
2022-04-15 02:50:12 +0100

    Adding additional functionality to getEquipment api call

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ app.get('/api/getEquipment:/userId', async (req, res) => {
         //   console.log('this ran' + response);
         //   res.status(200).json(response);
         // })
-        .catch(e => console.error(e.stack))
-    });
+      });
+    .catch(e => console.error(e.stack))
 });
 // BACKUP OF OLD SOLUTION, CHANGING TO PROMISE. DELETE AFTER!!!!
 // app.get('/api/getEquipment:/userId', async (req, res) => {
