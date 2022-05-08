ca97bb97ce1968ba971d017eed8965650e70ccd2
Rowan <up881930@myport.ac.uk>
2022-05-08 04:55:37 +0100

    Testing functionality for limiting reservations to one user

--- index.js
+++ index.js
@@ -136,8 +136,8 @@ app.put('/api/createReservation/:equipId/:userId', async (req, res) => {
 
         pool.query(checkIfUserHasReservation, checkIfUserHasReservationVals)
           .then (secondResponse => {
-            if (secondResponse.rows.length === 0) {
-              return (res.status(200).json('Error: You already have a reservation. Only one reservation can exist per user!')); 
+            if (response.rows.length === 0) {
+              return (res.status(200).json('Error: You already have a reservation. Only one reservation can exist per user!'));
             }
           })
 
