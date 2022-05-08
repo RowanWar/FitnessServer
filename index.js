8f195ba439cc2f81bbf1aa55bbe88a3d9ffac01e
Rowan <up881930@myport.ac.uk>
2022-05-08 04:13:35 +0100

    Implementing auto delete reservations x time after creation

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function deleteReservationById(equipId) {
     const equipmentQueryVals = [true, equipId];   // Passes in the id via the request paramter so it knows which reservation to amend
 
     pool.query(deleteReservationQuery, queryValues, (err, results) => {
-      if (err) {
-        return (console.log('Error encountered: ' + err)); // For security reasons, returns specific error to console-only
-      }
       .then (response => {
         pool.query(equipmentQuery, equipmentQueryVals, (err, results) => {
           console.log('Updated availablity to "True"');
         })
-      })
+      });
+      if (err) {
+        return (console.log('Error encountered: ' + err)); // For security reasons, returns specific error to console-only
+      }
 
       console.log('Ran query')
       console.log(results);
